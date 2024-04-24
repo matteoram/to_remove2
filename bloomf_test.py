84728d11146ffcf1f59053e1bd615d128071efae
matteoram <raminamatteo@me.com>
2024-04-24 19:03:31 +0200

    Create a new BloomFilter with two hash functions and relevant benchmark test
    Original BloomFilter object contains one hash function. This does not seem
    sufficient, since it has a false positive rate of over 9%, that is above
    the 5% level that we require. Original BloomFilter is not modified. Rather
    a new BloomFilter object is created in bloomf.py to test whether using two
    hash functions improves the false positive rate. Import library hashlib.
    
    Add a benchmark test for the new BloomFilter object. Test shows false
    positive rate of 0%. However, there must be an error in the way the two
    hash functions are combined in the new filter, because no string is
    indicated as probably present.

--- bloomf_test.py
+++ bloomf_test.py
@@ -44,3 +44,30 @@ false_positive_rate = false_positives / len(test_words)
 
 print(f'The percent of items not in the dataset is {round(len(word_absent)/len(test_words),4)*100}%')
 print(f'The false positive rate is {round(false_positive_rate,4)*100}%.')
+
+bloomf2 = BloomFilter(n, p)
+
+for item in word_present:
+    bloomf2.add(item)
+
+shuffle(word_present)
+shuffle(word_absent)
+
+test_words = word_present[:10] + word_absent
+shuffle(test_words)
+
+false_positives = 0
+for word in test_words:
+    if bloomf.check(word):
+        if word in word_absent:
+            false_positives += 1
+            print("'{}' is a false positive!".format(word))
+        else:
+            print("'{}' is probably present!".format(word))
+    else:
+        print("'{}' is definitely not present!".format(word))
+
+false_positive_rate = false_positives / len(test_words)
+
+print(f'The percent of items not in the dataset is {round(len(word_absent)/len(test_words),4)*100}%')
+print(f'The false positive rate is {round(false_positive_rate,4)*100}%.')
